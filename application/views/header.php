15fb1268d454f3810e48b4de18ac83b90ad869f7
Jimmy Pitts <jimmyrp@gmail.com>
2013-11-26 00:16:25 -0800

    stuff

--- application/views/header.php
+++ application/views/header.php
@@ -6,8 +6,8 @@
     <script src="<?= base_url('assets/js/bootstrap-select.min.js'); ?>"></script> 
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-select.min.css'); ?>"/>
 
-    <script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script> 
-    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="<?= base_url('assets/css/main.css'); ?>">
     <script src="<?= base_url('assets/js/main.js'); ?>"></script>
